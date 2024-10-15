e8936eb0dae7cab68d5961f33360529f5a83824b
SebastianEspende <espendesebastian@gmail.com>
2024-10-15 14:56:08 -0300

    pruebasserver

--- servidor/app.js
+++ servidor/app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+const port = 3000;
 var app = express();
 
 // view engine setup
@@ -21,6 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+//app.use('/GETFILES', filesRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -38,4 +39,5 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+app.listen(port, ()=> console.log(`Dolphin app listening on port ${port}!`))
 module.exports = app;
